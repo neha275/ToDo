51030b2789662adf2b9f0abbfd5f905cb1047881
Neha <neha.oscprofessionals@gmail.com>
2021-03-05 10:04:26 +0530

    add folder

---
+++ ToDo/Task/DataModel/Task+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  Task+CoreDataProperties.swift
+//  
+//
+//  Created by Neha Gupta on 04/03/21.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Task {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Task> {
+        return NSFetchRequest<Task>(entityName: "Task")
+    }
+
+    @NSManaged public var id: Int64
+    @NSManaged public var taskName: String?
+    @NSManaged public var taskdescription: String?
+    @NSManaged public var date: Date?
+
+}
